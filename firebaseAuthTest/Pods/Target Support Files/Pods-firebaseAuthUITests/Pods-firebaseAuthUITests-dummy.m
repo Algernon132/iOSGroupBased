b9b722f2d7301add772bc81e9e96e9ff2147d74c
Thomas Jurczyk <thomasjurczyk@outlook.com>
2019-04-11 17:56:33 -0500

    maybe now adding the firebase project worked

---
+++ firebaseAuthTest/Pods/Target Support Files/Pods-firebaseAuthUITests/Pods-firebaseAuthUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_firebaseAuthUITests : NSObject
+@end
+@implementation PodsDummy_Pods_firebaseAuthUITests
+@end
---
+++ firebaseAuthTest/firebaseAuthTest/signupViewController.swift
@@ -0,0 +1,57 @@
+//
+//  signupViewController.swift
+//  firebaseAuth
+//
+//  Created by Thomas Jurczyk on 4/11/19.
+//  Copyright © 2019 Thomas Jurczyk. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class signupViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var passwordConfirm: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func signupAction(_ sender: Any) {
+        if password.text != passwordConfirm.text {
+            let alertController = UIAlertController(title: "Password Incorrect", message: "Please re-type password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else{
+            Auth.auth().createUser(withEmail: email.text!, password: password.text!){ (user, error) in
+                if error == nil {
+                    self.performSegue(withIdentifier: "signupShowHome", sender: self)
+                }
+                else{
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
