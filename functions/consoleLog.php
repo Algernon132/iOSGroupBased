5892921a485deb859b558c181ac2504f1bb8e0d2
Chaz <c.bruscato97@gmail.com>
2019-05-14 19:27:07 -0500

    Disabled console logging

--- functions/consoleLog.php
+++ functions/consoleLog.php
@@ -1,7 +1,7 @@
 <?php
     function console_log( $data ){ //prints data to console. Very useful for debugging
-        echo '<script>';
-        echo 'console.log('. json_encode( $data ) .')';
-        echo '</script>';
+       // echo '<script>';
+       // echo 'console.log('. json_encode( $data ) .')';
+       // echo '</script>';
     }
 ?>
